63b392df40bd2347aa3c68207857f10a6fa6ef7a
PraveenSreevathsa <praveensreevathsa18@gmail.com>
2025-11-11 14:30:38 +0530

    Change greeting message from 'Pop!' to 'Chai!'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World Pop!'));
+app.get('/', (req, res) => res.send('Hello World Chai!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
