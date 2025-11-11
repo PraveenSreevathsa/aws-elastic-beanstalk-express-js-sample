ac959e4466bd0f96407ece413c8698aa277a77d8
PraveenSreevathsa <praveensreevathsa18@gmail.com>
2025-11-11 14:14:43 +0530

    Set up Express server and remove Fibonacci logic
    Removed Fibonacci function and added Express server.

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-// fibonacci.js
+const express = require('express');
+const app = express();
+const port = 8080;
 
-function generateFibonacci(n) {
-  const fib = [0, 1];
+app.get('/', (req, res) => res.send('Hello World Pop!'));
 
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
-
-  return fib.slice(0, n);
-}
-
-// Example: Generate first 10 numbers
-const n = 10;
-console.log(`Fibonacci series up to ${n} terms:`);
-console.log(generateFibonacci(n));
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
